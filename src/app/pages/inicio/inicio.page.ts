bdf730c04d7f467fc23152b4a9d4454d6df5106b
David Toledo <dtoledoz0207@gmail.com>
2019-05-28 22:56:01 -0500

    ionButton-Added

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -22,6 +22,11 @@ export class InicioPage implements OnInit {
       icon:"beaker",
       name:"Avatar",
       redirectTo: "/avatar"
+    },
+    {
+      icon:"radio-button-on",
+      name:"Botones y routes",
+      redirectTo: "/botones"
     }
   ];
 
