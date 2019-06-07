ec5f2c4eef6beb75d87a39533e65aa13f843d995
David Toledo <dtoledoz0207@gmail.com>
2019-06-06 22:50:24 -0500

    ionMenu-Added

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -84,6 +85,10 @@ export class InicioPage implements OnInit {
 
   ngOnInit() {
   }
+
+  /*toggleMenu(){
+    this.menuCtrl.toggle();
+  }*/
 
 }
 
