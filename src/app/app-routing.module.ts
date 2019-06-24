e2201489d1ea005c79297448e2d19fb205657925
David Toledo <dtoledoz0207@gmail.com>
2019-06-23 20:32:02 -0500

    ionSlides-Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,8 @@ const routes: Routes = [
   { path: 'progress-bar', loadChildren: './pages/progress-bar/progress-bar.module#ProgressBarPageModule' },
   { path: 'refresher', loadChildren: './pages/refresher/refresher.module#RefresherPageModule' },
   { path: 'searchbar', loadChildren: './pages/searchbar/searchbar.module#SearchbarPageModule' },
-  { path: 'segment', loadChildren: './pages/segment/segment.module#SegmentPageModule' }
+  { path: 'segment', loadChildren: './pages/segment/segment.module#SegmentPageModule' },
+  { path: 'slides', loadChildren: './pages/slides/slides.module#SlidesPageModule' }
 ];
 
 @NgModule({
